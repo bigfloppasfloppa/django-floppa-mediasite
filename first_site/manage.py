bbb3b67f005430e06ae634b8b7b09f802a541cd6
bigfloppasfloppa <109036318+bigfloppasfloppa@users.noreply.github.com>
2022-07-21 18:47:38 +0300

    added comment (line 6)

--- first_site/manage.py
+++ first_site/manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-
+# главный административный файл в проекте
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_site.settings')
